fd4b0f7f9a4cdfe752141efda62d700038c45e8a
Nick's Hardware Youtube Channel <56885781+nickshardware@users.noreply.github.com>
2021-08-17 19:08:01 -0400

    Simple Canvas Example

---
+++ pas2js/pas2js Tutorials/Demo2- Basic Canvas/Dynamic Canvas/test.pas
@@ -0,0 +1,39 @@
+// Dynamic Canvas Example
+Program test;
+  uses browserconsole,Web;
+
+var
+ canvas     : TJSHTMLCanvasElement;
+ ctx        : TJSCanvasRenderingContext2D;
+ divElement : TJSHTMLElement;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+Procedure drawSomething;
+begin
+  //create canvas element
+  canvas:=TJSHTMLCanvasElement(document.createElement('canvas'));
+  canvas.width:=400;
+  canvas.height:=400;
+  canvas.id:='canvas_2';
+  
+  //divElement:=TJSHTMLElement(document.getElementById('gameboard'));
+  //insert canvas element inside gameboard div
+  //divElement.appendChild(canvas);
+  // or insert inside body
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+  document.body.appendChild(canvas);
+  
+  ctx:=TJSCanvasRenderingContext2D(canvas.getContext('2d'));
+
+  ctx.fillStyle:='red';
+  ctx.fillRect(10, 10, 250, 100);
+  
+  ctx.fillStyle:='blue';
+  ctx.font:='48px serif';
+  ctx.fillText('Hello world', 20, 80);
+end;
+
+begin
+  writeln('Draw in Canvas');
+  drawSomething;
+end.
---
+++ pas2js/pas2js Tutorials/Demo2- Basic Canvas/Simple Canvas/test.pas
@@ -0,0 +1,35 @@
+// Simple Canvas Example
+Program test;
+  uses browserconsole,Web;
+
+var
+ canvas : TJSHTMLCanvasElement;
+ ctx    : TJSCanvasRenderingContext2D;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+Procedure drawSomething;
+begin
+  canvas:=TJSHTMLCanvasElement(document.getElementById('canvas_1'));
+  ctx:=TJSCanvasRenderingContext2D(canvas.getContext('2d'));
+  //canvas.width:=300;
+  //canvas.height:=300;
+
+  ctx.fillStyle := 'green';
+  //ctx.fillRect(10, 10, 150, 100);
+
+  //ctx.strokeStyle := 'red';
+  //ctx.font := '48px serif';
+  //ctx.strokeText('Hello Nick', 10, 50);
+
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.fill();
+
+end;
+
+begin
+  writeln('Draw in Canvas');
+  drawSomething;
+end.
